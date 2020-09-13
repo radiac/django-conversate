4bd5d0542aaaaba6d40a97b769ffe5ba00b5dd0d
Richard Terry <git@radiac.net>
2020-09-13 10:44:27 +0100

    Version 0.3.0

--- setup.py
+++ setup.py
@@ -1,40 +1,16 @@
-import os
+import re
+from pathlib import Path
 
-from setuptools import find_packages, setup
-
-from conversate import __version__
-
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+from setuptools import setup
 
 
-setup(
-    name="django-conversate",
-    version=__version__,
-    author="Richard Terry",
-    author_email="code@radiac.net",
-    description=(" Persistant chat for Django"),
-    license="BSD",
-    url="http://radiac.net/projects/django-conversate/",
-    long_description=read("README.rst"),
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.5",
-        "Framework :: Django",
-        "Framework :: Django :: 1.11",
-    ],
-    zip_safe=True,
-    packages=find_packages(exclude=("example*",)),
-    include_package_data=True,
-    install_requires=[
-        "Django>=2.2.0",
-        "django-yaa-settings",
-        "commonmark",
-        "emoji",
-    ],
-)
+def find_version(*paths):
+    path = Path(*paths)
+    content = path.read_text()
+    match = re.search(r"^__version__\s*=\s*['\"]([^'\"]*)['\"]", content, re.M)
+    if match:
+        return match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(version=find_version("conversate", "__init__.py"))
